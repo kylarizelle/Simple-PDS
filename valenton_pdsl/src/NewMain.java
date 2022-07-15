f5da439812ce6530d415558b5a29b6205013bf98
Kyla Rizelle Valenton <61926980+kylarizelle@users.noreply.github.com>
2022-07-15 17:54:32 +0800

    Add files via upload

---
+++ valenton_pdsl/src/NewMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jenny May
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LogIn().setVisible(true);
+    }
+    
+}
